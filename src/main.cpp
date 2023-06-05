3d4cd45105aacf88c1b5d26ff9ad845f12595b95
keykey7 <29210485+keykey7@users.noreply.github.com>
2023-06-05 22:21:14 +0200

    Blink LEDs with platformio

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+void setup() {
+    // initialize digital pin LED_BUILTIN as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
+
+}
+
+void loop() {
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(1000);                      // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    delay(1000);
+    Serial.println("Hello World!");
+}
